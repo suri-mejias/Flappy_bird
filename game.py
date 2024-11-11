2c974fa96b87492a5acca2f80eeac4edf173236d
suri-mejias <suri.mejias@ucas-edu.net>
2024-11-11 11:49:22 -0700

    did stuff

--- game.py
+++ game.py
@@ -13,6 +13,9 @@ class Game:
         self.gravity = 0.05
         self.bird_movement = 0
         self.rotated_bird = pygame.Surface((0,0))
+        self.pipes = []
+        self.pipe_height = [280, 425, 562]
+
 
     def resize_images(self):
         self.bird = pygame.transform.scale(self.bird, (51,34))
@@ -47,3 +50,32 @@ class Game:
     def flap(self):
         self.bird_movement = 0
         self.bird_movement -=2.5
+
+    def add_pipe(self):
+        random_pip_pos = random.choice(self.pipe_height)
+        bottom_pipe = self.pipe.get_rect(midtop = (600, random_pip_pos) )
+        top_pipe = self.pipe.get_rect(midbottom = (600, random_pip_pos - 211) )
+        self.pipes.append(bottom_pipe)
+        self.pipes.append(top_pipe)
+
+    def move_pipes(self):
+        for pipe in self.pipes:
+            pipe.centerx -= 1
+            if pipe.centerx <= -40:
+                self.pipes.remove(pipe)
+
+    def show_pipes(self, screen):
+        for pipe in self.pipes:
+            if pipe.bottom >= 700:
+                screen.blit(self.pipe,pipe)
+            else:
+                flip_pipe = pygame.transform.flip(self.pipe, False, True)
+                screen.blit(flip_pipe, pipe)
+
+    def check_collision(self):
+        for pipe in self.pipes:
+            if self.bird_rect.colliderect(pipe):
+                self.active = False
+
+        if self.bird_rect.top <= -100 or self.bird_rect.bottom >= 650:
+            self.active = False
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from game import Game
 pygame.init()
 screen = pygame.display.set_mode((400, 720))
 clock = pygame.time.Clock() 
+
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 2500)
+
 game = Game('img/bird.png', 'img/pipe.png', 'img/background.png', 'img/ground.png')
 game.resize_images()
 
@@ -18,11 +22,17 @@ while True:
             if event.key == pygame.K_SPACE and game.active:
                 game.flap()
 
+        if event.type == SPAWNPIPE:
+            game.add_pipe()
+
     game.show_background(screen)
 
     if game.active:
         game.show_bird(screen)
         game.update_bird()
+        game.move_pipes()
+        game.show_pipes(screen)
+        game.check_collision()
 
     game.show_ground(screen)
     game.move_ground()
